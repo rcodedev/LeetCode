d6156b1360d9dfed83d887ec6cdc343bf539c4aa
Rahul <152807952+rcodedev@users.noreply.github.com>
2026-08-14 10:50:49 +0530

    Time: 1 ms (99.95%), Space: 46.2 MB (90%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,13 +1,11 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        if (ransomNote.length() > magazine.length()) return false;
-        int[] alphabets_counter = new int[26];
-        for (char c : magazine.toCharArray())
-            alphabets_counter[c-'a']++;
-        for (char c : ransomNote.toCharArray()){
-            if (alphabets_counter[c-'a'] == 0) return false;
-            alphabets_counter[c-'a']--;
-        }
+        int[] counter = new int[128];
+        for (final char ch : magazine.toCharArray())
+            ++counter[ch];
+        for (final char ch : ransomNote.toCharArray())
+            if (--counter[ch] < 0)
+                return false;
         return true;
     }
 }
